176d9919163405fa0719277257a44310161dcd09
brianbentancourt <brianbentancourt9@gmail.com>
2018-12-21 00:41:02 -0300

    stylus-loader

---
+++ stylus-loader/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, './src/js/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: './dist/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					use: "css-loader"
+				})
+			},
+			{
+				test: /\.styl$/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						"css-loader",
+						{
+							loader: "stylus-loader",
+							options:{
+								use: [
+									require('nib'),
+									require('rupture')
+								],
+								import: [
+									'~nib/lib/nib/index.styl',
+									'~rupture/rupture/index.styl',
+								]
+							}
+						}
+					]
+				})
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ["@babel/env", "@babel/react"],
+						plugins: ["@babel/plugin-transform-runtime"]
+					}
+				}
+			},
+			{
+				test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/i,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 5000000000,
+							outputPath: './fonts',
+							publicPath: './fonts',
+							name: '[hash].[ext]'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(mp4|webm)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						limit: 100000000,
+						name: 'videos/[name].[ext]'
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("css/styles.css")
+	]
+}
